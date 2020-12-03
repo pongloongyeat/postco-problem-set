f14dcc3d7201e9b84306eb0f4e75e52bdb74cce9
pongloongyeat <31680656+pongloongyeat@users.noreply.github.com>
2020-12-03 16:58:29 +0800

    Branch selection (#24)
    * Create template branch selection
    
    * Use dropdown box to select branch
    
    * Convert branch_id to integer on save
    
    * Add branch controller
    
    * Add additional routes and route matching
    
    * Show all users on /branches
    
    * Dynamically get list from @branches
    
    * Add a branch_name entry
    
    * Show branch name on /branches
    
    * Add branch_name to edit and regsitration views
    
    * Permit branch_name as a form field
    
    * Use branch_name instead of email
    
    * Show branch ID on customers#index

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_11_28_122433) do
+ActiveRecord::Schema.define(version: 2020_12_03_083101) do
 
   create_table "branches", force: :cascade do |t|
     t.string "email", default: "", null: false
@@ -20,6 +20,7 @@ ActiveRecord::Schema.define(version: 2020_11_28_122433) do
     t.datetime "remember_created_at"
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
+    t.string "branch_name"
     t.index ["email"], name: "index_branches_on_email", unique: true
     t.index ["reset_password_token"], name: "index_branches_on_reset_password_token", unique: true
   end
@@ -30,22 +31,8 @@ ActiveRecord::Schema.define(version: 2020_11_28_122433) do
     t.float "temperature"
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
-    t.integer "user_id"
     t.integer "branch_id"
     t.index ["branch_id"], name: "index_customers_on_branch_id"
-    t.index ["user_id"], name: "index_customers_on_user_id"
-  end
-
-  create_table "users", force: :cascade do |t|
-    t.string "email", default: "", null: false
-    t.string "encrypted_password", default: "", null: false
-    t.string "reset_password_token"
-    t.datetime "reset_password_sent_at"
-    t.datetime "remember_created_at"
-    t.datetime "created_at", precision: 6, null: false
-    t.datetime "updated_at", precision: 6, null: false
-    t.index ["email"], name: "index_users_on_email", unique: true
-    t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
   end
 
 end
