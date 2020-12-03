d504c3591689dc47e444534ef5f496fecc708631
pongloongyeat <pongloongyeat@gmail.com>
2020-12-03 16:09:33 +0800

    Add additional routes and route matching

--- config/routes.rb
+++ config/routes.rb
@@ -1,8 +1,12 @@
 Rails.application.routes.draw do
-  get 'branches/index'
   devise_for :branches
   resources :customers
+
   root  'home#index'
+  match 'home', to: 'home#index', via: 'get'
+  
   get   'home/about'
+
+  match 'branches', to: 'branches#index', via: 'get'
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
